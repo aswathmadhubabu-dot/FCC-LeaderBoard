81f05a9c8a8a887a390067fa6b7edc462fcd276c
ases573 <ases573@gmail.com>
2017-06-25 15:39:25 +0530

    "[GIT AUTO COMMIT]: Sun Jun 25 2017 15:39:25 GMT+0530 (India Standard Time)"

--- src/App.jsx
+++ src/App.jsx
@@ -47,11 +47,18 @@ class App extends Component {
 
     render() {
         return (<div className='main'>
-        
-<button className='btn btn-success text-center' onClick={() => this.search()}>Recent &nbsp; <i class="fa fa-history" aria-hidden="true"></i>
+        <div class="card card-block">
+    <h4 class="card-title">Panel title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="flex-row">
+        <a class="card-link">Card link</a>
+        <a class="card-link">Another link</a>
+    </div>
+</div>
+<button className='btn btn-success text-center' onClick={() => this.search()}>Recent &nbsp; <i class="fa fa-history"></i>
 </button>
             <button className='btn btn-success text-center' onClick={() => this.searchall()}>
-All Time &nbsp; <i class="fa fa-rocket" aria-hidden="true"></i></button>
+All Time &nbsp; <i class="fa fa-rocket"></i></button>
             <div className="title text-center">FreeCodeCamp Leaderboard</div>
             <table className='table table-bordered display' id="mytable" cellSpacing="0" width="100%" data={this.state.users}>
                 <thead>
